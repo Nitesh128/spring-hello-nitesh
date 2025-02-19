f752ef13df5bee19b973aea8456158c384536f73
Nitesh128 <94357597+Nitesh128@users.noreply.github.com>
2025-02-19 16:11:34 +0530

    Update HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from Nitesh Sharma!";
+        return "Hello from Nitesh Sharma via GitOPS";
     }
 
 }
